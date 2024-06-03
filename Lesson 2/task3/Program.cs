af82fcc21b69f8d45cb61dd6e4b7fbf6bb1c402e
Alexey <lexa8keis@gmail.com>
2024-06-03 18:33:10 +0900

    add lesson2-task3

--- Lesson 2/task3/Program.cs
+++ Lesson 2/task3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int n = 10;
+// int[] array = { 2, 5, 4, 7, 8, 35, 1, 3, 8, 5 };
+// int i = 0;
+
+// while(i < n)
+// {
+//     if(array[i] % 2 == 0)
+//     {
+//         System.Console.Write($"{array[i]} ");
+//     }
+//     i = i + 1;
+// }
+
+
+int[] array = { 1, 2, 3, 5, 6, 7, 8, 9, 6, 3, 16, 18 ,100 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
